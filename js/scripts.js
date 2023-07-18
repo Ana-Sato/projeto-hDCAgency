b671a00c3780ea7fc1c9e327779cf6c5fb658ac0
Ana-Sato <ana.freire@edu.unipar.br>
2023-07-18 16:35:46 -0300

    Criei mais algumas secoes

--- js/scripts.js
+++ js/scripts.js
@@ -102,7 +102,9 @@ $(document).ready(function() {
 
     //Parallax
     setTimeout(function(){
-        $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'})
-    })
+        $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+        $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
+
+    }, 250);
     
 });
