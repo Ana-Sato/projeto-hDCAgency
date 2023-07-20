9ae0efcb986226e5a99b3ad0d9be6a63e4c48fb9
Ana-Sato <ana.freire@edu.unipar.br>
2023-07-20 11:21:37 -0300

    Criacao do scroll e correcoes no index

--- js/scripts.js
+++ js/scripts.js
@@ -144,4 +144,41 @@ $(document).ready(function() {
         }
     }
 
+    // scroll para secoes 
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicosSection = $('#servicos-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+
+        let btnId = $(this).attr('id');
+
+        console.log(btnId);
+
+        if(btnId == 'about-menu') {
+            scrollTo = aboutSection;
+        } else if(btnId == 'servicos-menu') {
+            scrollTo = servicosSection;
+        } else if(btnId == 'team-menu') {
+            scrollTo = teamSection;
+        } else if(btnId == 'portfolio-menu') {
+            scrollTo = portfolioSection;
+        } else if(btnId == 'contact-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top-70
+        }, 1500);
+    });
+
 });
